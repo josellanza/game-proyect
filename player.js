9bc89aec94f620777e56d59f20a13e97654cc16b
jose <jdellanza@gmail.com>
2018-06-29 12:16:06 +0200

    deploy4

--- player.js
+++ player.js
@@ -13,7 +13,7 @@ function Player (ctx) {
         x: 20,
         y: canvas.height/2 -110,
     }
-    this.speed = 8;
+    this.speed = 15;
     this.lives = 1;
     this.image = new Image(); 
     this.image.src = 'images/player1.png';
